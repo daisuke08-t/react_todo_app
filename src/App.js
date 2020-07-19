76ba823179e89bb3aad6f43f0da2cc679ddf7e61
dai03 <d.taniguchi0803@gmail.com>
2020-07-19 22:57:56 +0000

    create checktodo setState

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ const todos = [
   {id: 1, title: 'Task 1', isDone: false},
   {id: 2, title: 'Task 2', isDone: true},
   ];
+
+function TodoItem(props) {
+  return(
+    <li key={props.todo.id}>
+      <label>
+      <input type="checkbox" 
+      checked={props.todo.isDone}
+      onChange={() => props.checkTodo(props.todo)}
+      />
+      <span className={props.todo.isDone ? 'done' : ''}>
+      {props.todo.title}
+      </span>
+      </label>
+    </li>
+    );
+}
+
   
 function TodoList(props) {
   const todos = props.todos.map(todo => {
     return(
-      <li key={todo.id}>{todo.title}</li>
+      <TodoItem key={todo.id} todo={todo}
+      checkTodo={props.checkTodo}
+      />
       );
   });
   return(
@@ -27,12 +46,37 @@ class App extends React.Component {
     this.state = {
       todos: todos
     };
+    this.checkTodo = this.checkTodo.bind(this);
   }
+  
+  
+  checkTodo(todo) {
+    const todos = this.state.todos.map(todo => {
+      
+      return {id: todo.id, title: todo.title, isDone: todo.isDone};
+      
+    });
+    
+    const pos = this.state.todos.map(todo => {
+      return todo.id;
+      
+    }).indexOf(todo.id);
+    
+    todos[pos].isDone = !todos[pos].isDone;
+    this.setState({
+      todos: todos
+    });
+    
+  }
+  
+  
   render() {
     return(
       <div>
         <h1>My todo</h1>
-        <TodoList todos={this.state.todos}/>
+        <TodoList todos={this.state.todos}
+        checkTodo={this.checkTodo}
+        />
       </div>
       );
   }

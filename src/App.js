523f2b372ca81a3baa1a8820ff6f79a2cdc92617
dai03 <d.taniguchi0803@gmail.com>
2020-07-19 21:38:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const todos = [
+  {id: 0, title: 'Task 0', isDone: false},
+  {id: 1, title: 'Task 1', isDone: false},
+  {id: 2, title: 'Task 2', isDone: true},
+  ];
+  
+function TodoList(props) {
+  const todos = props.todos.map(todo => {
+    return(
+      <li key={todo.id}>{todo.title}</li>
+      );
+  });
+  return(
+    <ul>
+      {todos}
+    </ul>
+    );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    };
+  }
+  render() {
+    return(
+      <div>
+        <h1>My todo</h1>
+        <TodoList todos={this.state.todos}/>
+      </div>
+      );
+  }
 }
 
 export default App;
